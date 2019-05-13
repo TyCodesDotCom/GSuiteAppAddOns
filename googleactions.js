dbc77ed75a4c67ea3d898eee6575e81dd8948307
TyCodesDotCom <ty@tycodes.com>
2019-05-13 12:37:25 -0400

    Read Me Edit

--- googleactions.js
+++ googleactions.js
@@ -1,20 +1,11 @@
-// *********************************************************************
+// *************************************************************
 // ** Re-Enable & Disable Chromebook Form Functionality
 // ** G-Suit Google App Script Application
 // **
 // ** Author: Ty Christian
 // ** Date: 5/13/2019
 // **
-// ** Needed: customerId for google domain. Activate Admin Directory API
-// ** in the gscript console.
-// **
-// ** Description: Basic functionality build to allow for users with 
-// ** no administrative access to disable and re-enable chromebooks
-// ** at their own discretion providing they have the corresponding
-// ** serial number. Also has built in alerts that will send directly
-// ** to the submitters email following the desired action.
-// **
-// *********************************************************************
+// ************************************************************
 
 
 function onEdit(e) {
